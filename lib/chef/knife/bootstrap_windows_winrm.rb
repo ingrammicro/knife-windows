1882d39203b9c24965531c60be1b24c45362bb84
Samuel Patino <samuelp@softcom.com>
2018-04-24 16:42:05 +0200

    Allowing for unsecure winrm validationless bootstrap

--- lib/chef/knife/bootstrap_windows_winrm.rb
+++ lib/chef/knife/bootstrap_windows_winrm.rb
@@ -43,7 +43,7 @@ class Chef
         if (Chef::Config[:validation_key] && !File.exist?(File.expand_path(Chef::Config[:validation_key])))
           if !negotiate_auth? && !(locate_config_value(:winrm_transport) == 'ssl')
             ui.error("Validatorless bootstrap over unsecure winrm channels could expose your key to network sniffing")
-            exit 1
+            # exit 1
           end
         end
         bootstrap
